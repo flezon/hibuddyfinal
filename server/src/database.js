7dcb5147644b403ab82b6c87b5fe68e3626149e8
flezon <andres987_@hotmail.com>
2023-11-03 18:03:55 -0500

    add DB

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://localhost/hibuddy')
+    .connect('mongodb://0.0.0.0/hibuddy')
     .then((db) => console.log('Db is connected'))
     .catch((err) => console.log(err));
+
+    
